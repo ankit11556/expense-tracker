16042e01ae9142b60449f3b8284349db3dad0221
ankit <saharan442090@.com>
2025-03-23 17:05:41 +0530

    add navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import {Link} from "react-router-dom"
+const Navbar = () =>{
+  return(
+<nav className="bg-[#008080] text-[#F5F5DC] p-4 flex justify-between itmes-center">
+<h1 className="text-2xl font-bold">Expense Tracker</h1>
+
+  <ul className="flex gap-4 text-lg font-serif font-normal">
+    <li className="hover:bg-[#006666] p-2 rounded">
+      <Link to="/">Home</Link>
+    </li>
+    <li className="hover:bg-[#006666] p-2 rounded">
+      <Link to="/add">Add Expense</Link>
+    </li>
+    <li className="hover:bg-[#006666] p-2 rounded">
+      <Link to="/total">Total Balance</Link>
+    </li>
+  </ul>
+
+</nav>
+  )
+}
+
+export default Navbar
