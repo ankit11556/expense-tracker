aa06ac82dfc351f532f2b216ab3fbac32d7723da
ankit <saharan442090@gmail.com>
2025-04-06 16:13:07 +0530

    login route create

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,8 @@
 const User = require("../models/User");
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+//Signup
 exports.registerUser = async (req,res) => {
   try {
     const {name, email, password} = req.body;
@@ -18,5 +21,33 @@ exports.registerUser = async (req,res) => {
     res.status(201).json({message: "User registered successfully"})
   } catch (error) {
     res.status(500).json({ error: "Something went wrong" });
+  }
+}
+
+//Login
+exports.loginUser = async (req,res) => {
+  try {
+    const {email, password} = req.body;
+
+    const user = await User.findOne({email});
+    if(!user){
+      return res.status(404).json({error: "User not found"})
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({error: "Invalid credentials"})
+    }
+
+    //JWT token generate
+    const token = jwt.sign(
+      {userId: user._id, email: user.email},
+      process.env.JWT_KEY,
+      {expiresIn: "1d"}
+    );
+
+    res.status(200).json({message: "Login successful",token})
+  } catch (error) {
+    res.status(500).json({ error: "Login failed" });
   }
 }
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,6 +4,6 @@ const authRouter = express.Router();
 const authController = require('../controllers/authController')
 
 authRouter.post('/register',authController.registerUser)
-
+authRouter.post('/login',authController.loginUser);
 
 module.exports = authRouter
