d8cd865e1aa72ea5f32740f36579b83d4282bba4
ankit <saharan442090@gmail.com>
2025-04-08 22:30:02 +0530

    code update

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const transactionRouter = express.Router()
 const transactionController = require('../controllers/transactionController')
-import { verifyToken } from '../middlewares/verifyToken'
-transactionRouter.post("/add", verifyToken,transactionController.createTransaction)
-transactionRouter.get("/get", verifyToken,transactionController.getTransaction)
-transactionRouter.put("/edit/:id", verifyToken,transactionController.editTransaction)
-transactionRouter.delete("/delete/:id", verifyToken,transactionController.deleteTransaction)
+const  verifyToken = require('../middlewares/verifyToken')
+transactionRouter.post("/add", verifyToken.verifyToken,transactionController.createTransaction)
+transactionRouter.get("/get", verifyToken.verifyToken,transactionController.getTransaction)
+transactionRouter.put("/edit/:id", verifyToken.verifyToken,transactionController.editTransaction)
+transactionRouter.delete("/delete/:id", verifyToken.verifyToken,transactionController.deleteTransaction)
 module.exports = transactionRouter;
