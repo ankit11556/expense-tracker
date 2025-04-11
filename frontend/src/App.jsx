1f2b486cb8b2a748f2fd196c96098430449241bf
ankit <saharan442090@gmail.com>
2025-04-11 23:54:27 +0530

    create auth context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import AppRoutes from './routes/AppRoute'
 import './App.css'
-
+import { AuthProvider } from './context/AuthContext'
 function App() {
   
   return (
     <>
+    <AuthProvider>
       <AppRoutes></AppRoutes>
+      </AuthProvider>
     </>
   )
 }
--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,3 +1,16 @@
-const AuthContext = () =>{
-  
+import { useContext } from "react";
+
+import { createContext, useState }  from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({children})=>{
+  const [user,setUser] = useState(null);
+return(
+  <AuthContext.Provider value={{user,setUser}}>
+    {children}
+  </AuthContext.Provider>
+)
 }
+
+export const useAuth = () => useContext(AuthContext)
