d350f1131585a48f8c15237c153b9d16fdde48fd
ankit <saharan442090@gmail.com>
2025-04-29 22:20:45 +0530

    remove a log

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -145,6 +145,7 @@ exports.loginUser = async (req,res) => {
     const {email, password} = req.body;
 
     const user = await User.findOne({email});
+    
     if(!user){
       return res.status(404).json({error: "User not found"})
     }
